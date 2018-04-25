7a9cb4896ecd96436a9e15090ed516d4d6dd5dec
Lauren Light <laurenmlight@ufl.edu>
2018-04-24 23:34:01 -0400

    add jquery accordion menu

--- js/main.js
+++ js/main.js
@@ -30,6 +30,21 @@ $( document ).ready(function() {
 // Backstretch Plugin
     $("#about").backstretch("img/fruitypebbles2.jpg");
 
+
+//Custom JQuery Accordion Menu
+
+    $(".accordion h3:first").addClass("active");
+    $(".accordion p").hide();
+
+    $(".accordion h3").click(function(){
+
+    $(this).next("p").slideToggle("slow")
+    .siblings("p:visible").slideUp("slow");
+    $(this).toggleClass("active");
+    $(this).siblings("h3").removeClass("active");
+
+  });
+
 });
 
 
